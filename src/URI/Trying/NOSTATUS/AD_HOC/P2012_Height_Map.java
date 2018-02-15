00308faa3c6675cee2fb349d041d9f6dbafe6bb0
Boombarm <boombarm@gmail.com>
2018-02-15 12:04:30 +0700

    15_2_2018_#2

---
+++ src/URI/Trying/NOSTATUS/AD_HOC/P2012_Height_Map.java
@@ -0,0 +1,25 @@
+/**
+ * @author Teerapat Phokhonwong
+ * @Onlinejudge:
+ * @Problem: 
+ * @Link: 
+ * @Timelimit: 1 sec
+ * @Status: 
+ * @Solution:
+ * @Note: 
+ */
+
+package URI.Trying.NOSTATUS.AD_HOC;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class P2012_Height_Map {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+
+}
---
+++ src/URI/Trying/NOSTATUS/BEGINNER/P2674_Super_Primes_Engage.java
@@ -0,0 +1,20 @@
+package URI.Trying.NOSTATUS.BEGINNER;
+
+import java.io.*;
+
+/**
+ * Created by teerapat on 2/7/2018.
+ */
+public class P2674_Super_Primes_Engage {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String in;
+        while ((in = br.readLine()) != null) {
+
+        }
+        bw.flush();
+    }
+}
