9504aeff7e9c701f2b0dff561af9d31a01cba874
Boombarm <boombarm@gmail.com>
2018-02-19 18:41:10 +0700

    19-02-2018

--- src/URI/Trying/NOSTATUS/AD_HOC/P1085_The_Club_Ballroom.java
+++ src/URI/Trying/NOSTATUS/AD_HOC/P1085_The_Club_Ballroom.java
@@ -7,7 +7,7 @@
  * @Status:
  * @Submission:
  * @Runtime:
- * @Solution: shellSort + Greedy algorithm
+ * @Solution: QuickSort + Greedy algorithm
  * @Note:
  */
 package URI.Trying.NOSTATUS.AD_HOC;
@@ -17,13 +17,124 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
 
 public class P1085_The_Club_Ballroom {
 
-    static int[] plankWidth, plankHigth;
-    static LinkedList<Integer> pickedW, pickedH; 
+
+    static SortedList plankList = new SortedList();
     static int n, m, l, k;
+
+    static class SortedList {
+        Node firstNode;
+        Node lastNode;
+
+
+        public Node getFirstNode() {
+            return firstNode;
+        }
+
+        public Node getLastNode() {
+            return lastNode;
+        }
+
+        public void clear() {
+            firstNode = null;
+            lastNode = null;
+        }
+
+        // >
+        public void add_DESC(int data) {
+            if (firstNode == null) {
+                firstNode = new Node();
+                firstNode.data = data;
+                lastNode = firstNode;
+            } else if (firstNode.compare_DESC(data)) {
+                Node node = new Node();
+                node.data = data;
+                node.next = firstNode;
+                firstNode = node;
+            } else {
+                Node cur = firstNode;
+                Node prev = null;
+                loop:
+                while (cur != null) {
+                    if (cur.compare_DESC(data)) {
+                        break loop;
+                    }
+                    prev = cur;
+                    cur = cur.next;
+                }
+                Node node = new Node();
+                node.data = data;
+                prev.next = node;
+                node.next = cur;
+            }
+        }
+
+        // <
+        public void add_ASC(int data) {
+            if (firstNode == null) {
+                firstNode = new Node();
+                firstNode.data = data;
+                lastNode = firstNode;
+            } else if (firstNode.compare_ASC(data)) {
+                Node node = new Node();
+                node.data = data;
+                node.next = firstNode;
+                firstNode = node;
+            } else if (lastNode.compare_ASC(data)) {
+                Node node = new Node();
+                node.data = data;
+                lastNode.next = node;
+                lastNode = node;
+            } else {
+                Node cur = firstNode;
+                Node prev = null;
+                loop:
+                while (cur != null) {
+                    if (cur.compare_ASC(data)) {
+                        break loop;
+                    }
+                    prev = cur;
+                    cur = cur.next;
+                }
+                Node node = new Node();
+                node.data = data;
+                prev.next = node;
+                node.next = cur;
+            }
+        }
+
+        public void remove(Node node) {
+            if (node.equals(firstNode)) {
+                firstNode = firstNode.next;
+                return;
+            }
+            Node cur = firstNode;
+            Node prev = null;
+            loop:
+            while (!cur.equals(node)) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        public boolean compare_ASC(int b) {
+            return data > b;
+        }
+
+        public boolean compare_DESC(int b) {
+            return data < b;
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,6 +142,7 @@ public class P1085_The_Club_Ballroom {
         String input;
         while (!(input = br.readLine()).equals("0 0")) {
             String[] st = input.split(" ");
+
             n = Integer.parseInt(st[0]);//width
             m = Integer.parseInt(st[1]);//higth
             l = Integer.parseInt(br.readLine());//1 - 100 cm 
@@ -41,23 +153,21 @@ public class P1085_The_Club_Ballroom {
             if ((l * k) >= width) {
                 int c1 = 0;
                 int c2 = 0;
-                plankWidth = new int[k];
-                plankHigth = new int[k];
                 for (int i = 0; i < k; i++) {
                     int plank = Integer.parseInt(st[i]);
-                    if (plank <= n) {
-                        plankWidth[c1++] = plank;
-                    }
-                    if (plank <= m) {
-                        plankHigth[c2++] = plank;
-                    }
+                    plankList.add_DESC(plank);
                 }
-                shellSort(plankWidth);
-                shellSort(plankHigth);
-                for (int i = 0; i < c1; i++) {
-                    System.out.print(plankWidth[i] + " ");
+                //Iterator Node
+                Node cur = plankList.getFirstNode();
+                while (cur != null) {
+                    System.out.print(" " + cur.data);
+                    cur = cur.next;
                 }
-                System.out.println("");
+                System.out.println();
+
+
+                //Test Remove
+
                 //width
                 int usedA = 0, usedB = 0;
                 for (int i = 0; i < c1; i++) {
@@ -70,6 +180,7 @@ public class P1085_The_Club_Ballroom {
                 answer = usedA < usedB ? usedA : usedB;
             }
             bw.append((answer > 0 ? answer : "impossivel") + "\n");
+            plankList.clear();
         }
         bw.flush();
     }
