cc0a9f3c3c072d1af425b588a29555f9d5a70829
Boombarm <boombarm@gmail.com>
2018-02-20 18:10:45 +0700

    20-02-2018

--- src/URI/Trying/WA/P1569_Snakes_and_Ladders.java
+++ src/URI/Trying/WA/P1569_Snakes_and_Ladders.java
@@ -18,10 +18,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class P1569_Snakes_and_Ladders {
 
@@ -39,47 +36,61 @@ public class P1569_Snakes_and_Ladders {
             int players = Integer.parseInt(st[0]);
             int total_snake_ladder = Integer.parseInt(st[1]);
             int dice = Integer.parseInt(st[2]);
-            playersIndex = new int[players];
+            playersIndex = new int[players + 1];
             Arrays.fill(playersIndex, 1);
             snake_ladder = new HashMap();
             while (total_snake_ladder-- > 0) {
                 st = br.readLine().split(" ");
                 int start = Integer.parseInt(st[0]);
                 int end = Integer.parseInt(st[1]);
-                snake_ladder.put(start, end);
-                if (end != 100) {
+                if(start < 100 && end < 100) {
+                    snake_ladder.put(start, end);
                     snake_ladder.put(end, start);
                 }
             }
-            moveList = new LinkedList();
-            for (int i = 0; i < dice; i++) {
-                moveList.add(Integer.parseInt(br.readLine()));
-            }
-            int i = 0;
-            Iterator<Integer> listIterator = moveList.iterator();
-            while (listIterator.hasNext()) {
-                int nextIndex = playersIndex[i] + listIterator.next();
-                Integer head_tail = snake_ladder.get(nextIndex);
-                if (head_tail != null) {
-                    nextIndex = head_tail;
-                }
 
-                playersIndex[i] = nextIndex;
-                if (playersIndex[i] >= 100) {
-                    playersIndex[i] = 100;
-                    break;
-                }
+            boolean end = false;
+            int p = 1;
+            int x;
+            for (int i = 0; i < dice; ++i, ++p) {
+                x = Integer.parseInt(br.readLine());
 
-                if (++i == players) {
-                    i = 0;
+                if (end) continue;
+                if (p > players) p = 1;
+
+                while (true) {
+                    playersIndex[p] += x;
+                    playersIndex[p] = min(playersIndex[p], 100);
+
+                    if (playersIndex[p] == 100) {
+                        end = true;
+                        break;
+                    }
+
+                    Integer tail = snake_ladder.get(playersIndex[p]);
+                    if (tail != null) {
+                        playersIndex[p] = tail;
+                    }
+
+                    if (playersIndex[p] == 100) {
+                        end = true;
+                        break;
+                    }
+
+                    if (snake_ladder.get(playersIndex[p]) == null) break;
                 }
             }
-            i = 1;
-            for (; i <= players; i++) {
-                bw.append("Position of player " + i + " is " + playersIndex[i - 1] + ".\n");
+
+            for (int i = 1; i <= players; i++) {
+                bw.append("Position of player " + i + " is " + playersIndex[i] + ".\n");
             }
         }
+
         bw.flush();
+    }
+
+    static int min(int p, int min) {
+        return p > 100 ? 100 : p;
     }
 
 }
