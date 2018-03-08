f433cb94c97c4c28c99bf206a8526edac27f970d
Boombarm <boombarm@gmail.com>
2018-03-08 11:50:41 +0700

    08032018

---
+++ src/AIZU/Trying/ALDS1/ALDS1_3_A_Stack.java
@@ -0,0 +1,28 @@
+package AIZU.Trying.ALDS1;
+
+/**
+ * @author Teerapat Phokhonwong
+ * @Onlinejudge: AIZU ONLINE JUDGE
+ * @Categories:
+ * @Problem:
+ * @Link:
+ * @Timelimit: 1 sec
+ * @Status:
+ * @Memory:
+ * @Submission:
+ * @Runtime:
+ * @Solution:
+ * @Note:
+ */
+
+import java.io.*;
+
+public class ALDS1_3_A_Stack {
+
+    public static void main(String a[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+
+}
---
+++ src/AIZU/Trying/ITP1/ITP1_1_C_Rectangle.java
@@ -0,0 +1,29 @@
+package AIZU.Trying.ITP1;
+
+/**
+ * @author Teerapat Phokhonwong
+ * @Onlinejudge: AIZU ONLINE JUDGE
+ * @Categories:
+ * @Problem:
+ * @Link:
+ * @Timelimit: 1 sec
+ * @Status:
+ * @Memory:
+ * @Submission:
+ * @Runtime:
+ * @Solution:
+ * @Note:
+ */
+
+import java.io.*;
+
+public class ITP1_1_C_Rectangle {
+
+    public static void main(String a[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] st = br.readLine().split(" ");
+    }
+
+
+}
