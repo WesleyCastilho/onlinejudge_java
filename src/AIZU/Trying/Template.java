4a0136ade8a69ca4143e41ee029a2fdc110b84d7
Boombarm <boombarm@gmail.com>
2018-03-15 11:48:44 +0700

    15042018

--- src/AIZU/Trying/Template.java
+++ src/AIZU/Trying/Template.java
@@ -15,7 +15,11 @@ package AIZU.Trying;
  * @Note:
  */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Template {
 
