0e9ec89815485fb73447e59e427e70fa316684b0
Boombarm <boombarm@gmail.com>
2018-03-18 21:53:20 +0700

    18032018

---
+++ src/URI/Main.java
@@ -0,0 +1,16 @@
+package URI;
+
+import URI.Trying.TLE.P1883_Escape_From_Ayutthaya;
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        P1883_Escape_From_Ayutthaya P1883 = new P1883_Escape_From_Ayutthaya();
+    }
+
+}
+
+
--- src/URI/Trying/TLE/P1883_Escape_From_Ayutthaya.java
+++ src/URI/Trying/TLE/P1883_Escape_From_Ayutthaya.java
@@ -1,199 +1,211 @@
-/**
- * @author Teerapat Phokhonwong
- * @Onlinejudge: URI Online Judge
- * @Problem: 1883 Escape From Ayutthaya
- * @Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1883
- * @Timelimit: 1 sec
- * @Status: Time limit exceeded
- * @Submission: 2/15/16, 10:06:56 PM
- * @Runtime: 2.000
- * @Solution: customize A* Algorithm for find shortest path and simulation
- * @Note:
- */
 package URI.Trying.TLE;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+
+import java.io.*;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class P1883_Escape_From_Ayutthaya {
 
-    private static char[][] map;
-    private static boolean[][] closed;
-    private static int[][] heuristic;
-    private static int[][] movementCost;
-    private static int n, m;
-    private static Point startPoint, goalPoint, fire;
-    private static LinkedList<Point> spread;
-    private static LinkedList<Point> shortedPath;
-    private static Node openList_Header;
+    BufferedReader br;
+    BufferedWriter bw;
+    int T, N, M;
+    int[][] MAP;
+    Point start, end, fire;
 
-    private static class Point {
+    private class Point {
+        int x;
+        int y;
+        Point p;
+        char direct;
 
-        int x, y;
-
-        public Point(int x, int y) {
+        public Point(int y, int x) {
             this.x = x;
             this.y = y;
         }
 
     }
 
-    private static class Node {
+    private static Deque<Point> spread, move;
+    int[][] heuristic;
 
-        Node Parent;
-        Node next;
-        int value;
-        Point point;
 
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-    private static void addNode(int value, Point point) {
-        Node node = new Node();
-        node.value = value;
-        node.point = point;
-        if (openList_Header == null) {
-            openList_Header = node;
-        } else {
-            Node cur = openList_Header;
-            Node prev = null;
-            while (cur.next != null) {
-
-                cur = cur.next;
-            }
-            cur.next = node;
-        }
-    }
-
-    private static void removeNode() {
-        openList_Header = openList_Header.next;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        OutputStream out = new BufferedOutputStream(System.out);
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            spread = new LinkedList();
-            shortedPath = new LinkedList();
+    public P1883_Escape_From_Ayutthaya() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            spread = new LinkedList<Point>();
+            move = new LinkedList<Point>();
             String[] st = br.readLine().split(" ");
-            n = Integer.parseInt(st[0]);
-            m = Integer.parseInt(st[1]);
-            map = new char[n][m];
-            heuristic = new int[n][m];
-            movementCost = new int[n][m];
-            for (int y = 0; y < n; y++) {
-                map[y] = br.readLine().toCharArray();
-                for (int x = 0; x < m; x++) {
-                    switch (map[y][x]) {
+            N = Integer.parseInt(st[0]);
+            M = Integer.parseInt(st[1]);
+            MAP = new int[N][M];
+            heuristic = new int[N][M];
+            for (int y = 0; y < N; y++) {
+                char[] c = br.readLine().toCharArray();
+                for (int x = 0; x < M; x++) {
+                    switch (c[x]) {
                         case '#':
-                            map[y][x] = '#';
-                            closed[y][x] = true;
+                            MAP[y][x] = 1;
                             break;
                         case 'S':
-                            startPoint = new Point(x, y);
-                            closed[y][x] = true;
+                            MAP[y][x] = 1;
+                            start = new Point(y, x);
+                            move.add(start);
                             break;
                         case 'E':
-                            goalPoint = new Point(x, y);
+                            MAP[y][x] = Integer.MIN_VALUE;
+                            end = new Point(y, x);
                             break;
                         case 'F':
-                            fire = new Point(x, y);
+                            fire = new Point(y, x);
+                            fire.direct = 'A';
                             spread.addFirst(fire);
-                            closed[y][x] = true;
                             break;
                     }
                 }
             }
-            getHeuristic();
-            boolean possible = true;
-            if (heuristic[startPoint.y][startPoint.x] >= heuristic[fire.y][fire.x]) {
-                possible = false;
-            } else if (!AStarSearch()) {
-                possible = false;
-            } else if (simulate()) {
-                possible = false;
+            int gameOver = (Math.abs(fire.x - end.x) + Math.abs(fire.y - end.y));
+            int minMove = (Math.abs(start.x - end.x) + Math.abs(start.y - end.y));
+            if (minMove > gameOver) {
+                bw.append("N");
+            } else {
+
+                boolean escaped = simulation();
+                bw.append(escaped ? "Y" : "N");
             }
-            out.write(((possible ? "Y" : "N") + "\n").getBytes());
+            bw.newLine();
+            bw.flush();
         }
-        out.flush();
     }
 
-    private static boolean AStarSearch() {
-        boolean posible = false;
-        openList_Header = null;
-        int h = 0;
-        int g = 0;
-        int x = 0;
-        int y = 0;
-        addNode(h + g, startPoint);
-        while (openList_Header != null) {
-            Point p = openList_Header.point;
-            removeNode();
-            //Left
-            x = p.x - 1;
-            y = p.y;
-            if (map[y][x] != '#') {
+    void buildHeuristic()
 
-            }
-            //rigth
-            x = p.x + 1;
-            y = p.y;
-            if (map[y][x] != '#') {
-
-            }
-            //top
-            x = p.x;
-            y = p.y - 1;
-            if (map[y][x] != '#') {
-
-            }
-            //down
-            x = p.x;
-            y = p.y + 1;
-            if (map[y][x] != '#') {
-
-            }
-            
-        }
-
-        return posible;
-    }
-
-    private static boolean simulate() {
-        boolean canEscape = true;
-        return canEscape;
-    }
-
-    private static int calculateHeuristic(int x, int y) {
-        int dx = Math.abs(x - goalPoint.x);
-        int dy = Math.abs(y - goalPoint.y);
-        return dx + dy;
-    }
-
-    private static void getHeuristic() {
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < m; x++) {
-                switch (map[y][x]) {
-                    case '#':
-                    case 'E':
-                        break;
-                    default:
-                        heuristic[y][x] = calculateHeuristic(x, y);
-                        break;
+    private boolean simulation() {
+        while (true) {
+            int c1 = 0;
+            int n1 = spread.size();
+            Point fire;
+            while (c1 < n1) {
+                fire = spread.remove();
+                if (fire.x == end.x && fire.y == end.y) {
+                    return false;
                 }
+                setSpread(fire);
+                c1++;
+            }
+
+            int c2 = 0;
+            int n2 = move.size();
+            Point m;
+            while (c2 < n2) {
+                m = move.remove();
+                if (m.x == end.x && m.y == end.y) {
+                    return true;
+                } else if (MAP[m.y][m.x] != -1) {
+                    setNextMove(m);
+                }
+                c2++;
+            }
+            if (move.isEmpty()) {
+                return false;
             }
         }
     }
+
+    private void setSpread(Point fire) {
+        int x = fire.x;
+        int y = fire.y;
+        MAP[y][x] = -1;
+        switch (fire.direct) {
+            case 'A':
+                spreadRight(y, x);
+                spreadLeft(y, x);
+                spreadBot(y, x);
+                spreadTop(y, x);
+                break;
+            case 'R':
+                spreadRight(y, x);
+                spreadBot(y, x);
+                spreadTop(y, x);
+                break;
+            case 'L':
+                spreadLeft(y, x);
+                spreadBot(y, x);
+                spreadTop(y, x);
+                break;
+            case 'B':
+                spreadRight(y, x);
+                spreadBot(y, x);
+                spreadLeft(y, x);
+                break;
+            case 'T':
+                spreadTop(y, x);
+                spreadLeft(y, x);
+                spreadRight(y, x);
+                break;
+        }
+
+    }
+
+    void spreadRight(int y, int x) {
+        if (x + 1 < M) {
+            Point p = new Point(y, x + 1);
+            p.direct = 'R';
+            spread.addLast(p);
+        }
+    }
+
+    void spreadLeft(int y, int x) {
+        if (x > 0) {
+            Point p = new Point(y, x - 1);
+            p.direct = 'L';
+            spread.addLast(new Point(x - 1, y));
+        }
+    }
+
+    void spreadBot(int y, int x) {
+        if (y + 1 < N) {
+            Point p = new Point(y + 1, x);
+            p.direct = 'B';
+            spread.addLast(p);
+        }
+    }
+
+    void spreadTop(int y, int x) {
+        if (y > 0) {
+            Point p = new Point(y - 1, x);
+            p.direct = 'T';
+            spread.addLast(p);
+        }
+    }
+
+    private void setNextMove(Point moved) {
+        int x = moved.x;
+        int y = moved.y;
+        MAP[y][x] = 1;
+
+        //Move left
+        if (x > 0 && MAP[y][x - 1] != 1) {
+            move.addLast(new Point(y, x - 1));
+        }
+
+        //Move right
+        if ((x + 1) < M && MAP[y][x + 1] != 1) {
+            move.addLast(new Point(y, x + 1));
+        }
+
+        //Move top
+        if (y > 0 && MAP[y - 1][x] != 1) {
+            move.addLast(new Point(y - 1, x));
+        }
+
+        //Move bottom
+        if (y + 1 < N && MAP[y + 1][x] != 1) {
+            move.addLast(new Point(y + 1, x));
+        }
+    }
+
 
 }
