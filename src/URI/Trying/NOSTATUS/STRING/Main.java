5378a2325c6d4162eafbaadbffefad3bd7f12da0
Boombarm <boombarm@gmail.com>
2018-03-25 13:39:44 +0700

    25032018

--- src/URI/Trying/NOSTATUS/STRING/Main.java
+++ src/URI/Trying/NOSTATUS/STRING/Main.java
@@ -1,14 +1,14 @@
 package URI.Trying.NOSTATUS.STRING;
 
 
-import URI.Trying.WA.P1912_Help_Seu_Madruga;
+
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new P1912_Help_Seu_Madruga();
+
     }
 
 }
