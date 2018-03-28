f3d280ffbbdc5a3b34aa4629a34ff0741c8f7f07
Boombarm <boombarm@gmail.com>
2018-03-28 18:17:33 +0700

    28032018

--- src/URI/Trying/WA/P2684_Help_Professor_Webscript.java
+++ src/URI/Trying/WA/P2684_Help_Professor_Webscript.java
@@ -18,80 +18,109 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.LinkedList;
 
 public class P2684_Help_Professor_Webscript {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<String> stack = new LinkedList<String>();
         int n = Integer.parseInt(br.readLine());
         String input;
-        while (n-- > 0) {
+        while (n > 0) {
             input = br.readLine();
             int type = Integer.parseInt(input.substring(0, 1));
             String html = input.substring(2, input.length());
             char[] c = html.toCharArray();
             int size = c.length;
-            int tags = 0;
-            if (type == 1) {
-                char last = ' ';
-                loop:
-                for (int i = 0; i < size; i++) {
-                    switch (c[i]) {
-                        case '/':
-                            if (last == '<' || last == '>') {
-                                tags = -1;
-                                break loop;
-                            }
-                            break;
-                        case '<':
-                            tags++;
-                            break;
-                        case '>':
-                            tags--;
-                            break;
-                        default:
-                            break;
+            int count;
+            switch (type) {
+                case 1:
+                    count = 0;
+                    for (int i = 0; i < size; i++) {
+                        switch (c[i]) {
+                            case '<':
+                                count++;
+                                break;
+                            case '>':
+                                count--;
+                                break;
+                        }
                     }
-                    last = c[i];
-                }
-                bw.append((tags == 0 ? "Successful!!" : "error") + "\n");
-            } else {
-                boolean isOpenTag = false;
-                boolean isCloseTag = false;
-                loop:
-                for (int i = 0; i < size; i++) {
-                    switch (c[i]) {
-                        case '<':
-                            int j = i + 1;
-                            if (j < size && c[j] == '/') {
-                                isCloseTag = true;
-                                i = j;
-                                continue loop;
+                    bw.append(count == 0 ? "Successful!!\n" : "error\n");
+                    break;
+                case 0:
+                    boolean successful = true;
+                    count = 0;
+                    loop:
+                    for (int i = 0; i < size - 2; i++) {
+                        if (c[i] == '<') {
+                            count++;
+                            if (c[i + 1] == '/') {
+                                String s = "<";
+                                boolean error = true;
+                                int j = i + 1;
+                                for (; j < size; j++) {
+                                    if (c[j] == '>') {
+                                        error = false;
+                                        if (stack.isEmpty() || !stack.pollLast().startsWith(s)) {
+//                                        bw.append("error s= " + s + "\n");
+                                            successful = false;
+                                            break loop;
+                                        }
+                                        s = "";
+                                        count--;
+                                    }
+                                    s += c[j];
+                                }
+                                if (error) {
+                                    successful = false;
+                                    break loop;
+                                }
+                                i = j - 1;
+                            } else {
+                                String s = "<";
+                                boolean error = true;
+                                boolean getText = true;
+                                int j = i + 1;
+                                for (; j < size; j++) {
+                                    if (c[j] == ' ') {
+                                        getText = false;
+                                    }
+                                    if (getText) {
+                                        s += c[j];
+                                    }
+                                    if (c[j] == '>') {
+                                        if (!getText) {
+                                            s += '>';
+                                        }
+                                        if (count == 0) {
+                                            successful = false;
+                                            break loop;
+                                        }
+                                        count--;
+                                        error = false;
+                                        stack.add(s);
+                                        bw.append("add " + s + "\n");
+                                        s = "";
+                                    }
+                                }
+                                if (error) {
+                                    successful = false;
+                                    break loop;
+                                }
+                                i = j - 1;
                             }
-                            isOpenTag = true;
-                            i = j;
-                            break;
-                        case '>':
-                            if (!isCloseTag && !isOpenTag) {
-                                tags = -1;
-                                break loop;
-                            }
-                            if (isCloseTag) {
-                                tags--;
-                                isCloseTag = false;
-                            }
-                            if (isOpenTag) {
-                                tags++;
-                                isOpenTag = false;
-                            }
-                            break;
-                        default:
-                            break;
+                        }
                     }
-                }
-                bw.append((tags == 0 && !isOpenTag && !isCloseTag ? "Successful!!" : "error") + "\n");
+                    bw.append(successful && stack.size() == 0 ? "Successful!!\n" : "error\n");
+                    break;
             }
+            if (--n == 0) {
+                break;
+            }
+            stack.clear();
         }
         bw.flush();
     }
