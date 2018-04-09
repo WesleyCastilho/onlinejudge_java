ac77cca9d3787be0f6bc09e60ffce5f80c3c554f
Boombarm <boombarm@gmail.com>
2018-04-09 13:49:43 +0700

    09042018

--- src/URI/Trying/NOSTATUS/AD_HOC/Main.java
+++ src/URI/Trying/NOSTATUS/AD_HOC/Main.java
@@ -1,13 +1,12 @@
 package URI.Trying.NOSTATUS.AD_HOC;
 
 
-
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        new P1657_Automatic_Correction_of_Misspellings();
     }
 
 }
---
+++ src/URI/Trying/NOSTATUS/AD_HOC/P1657_Automatic_Correction_of_Misspellings.java
@@ -0,0 +1,36 @@
+package URI.Trying.NOSTATUS.AD_HOC;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class P1657_Automatic_Correction_of_Misspellings {
+
+    public P1657_Automatic_Correction_of_Misspellings() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Set<String> dictionary = new TreeSet<String>();
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            String word = br.readLine();
+            dictionary.add(word);
+        }
+        int q = Integer.parseInt(br.readLine());
+        while (q-- > 0) {
+            String word = br.readLine();
+            if (dictionary.contains(word)) {
+                bw.append(word + " is correct\n");
+            } else {
+                bw.append(word + " is a misspelling of " + "\n");
+            }
+        }
+        bw.flush();
+    }
+
+
+}
