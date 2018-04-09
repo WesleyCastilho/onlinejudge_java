2f98bfd23d43dbd5339ba812bf095ed3adcfc179
Boombarm <boombarm@gmail.com>
2018-04-09 22:44:44 +0700

    09042018

--- src/URI/Trying/WA/Main.java
+++ src/URI/Trying/WA/Main.java
@@ -2,12 +2,14 @@ package URI.Trying.WA;
 
 
 
+
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new P2684_Help_Professor_Webscript();
+
     }
 
 }
