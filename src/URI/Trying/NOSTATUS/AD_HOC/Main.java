16e765bfd62318aa93d4bd7c804323702c079190
Teerapat_ <boombarm@gmail.com>
2018-09-07 21:11:03 +0700

    07-09-2018

--- src/URI/Trying/NOSTATUS/AD_HOC/Main.java
+++ src/URI/Trying/NOSTATUS/AD_HOC/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new P1657_Automatic_Correction_of_Misspellings();
+        new P2693_Van();
     }
 
 }
