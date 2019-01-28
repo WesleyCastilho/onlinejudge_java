3e52633b49c03789ad6e545a3c5948235c99976a
Teerapat <boombarm@gmail.com>
2019-01-29 00:19:44 +0700

    29-01-2019

---
+++ src/URI/Trying/NOSTATUS/PARADIGMS/P1034_Ice_Statues_Festival.java
@@ -0,0 +1,35 @@
+package URI.Trying.NOSTATUS.PARADIGMS;
+
+
+/**
+ * @author Teerapat Phokhonwong
+ * @Onlinejudge: URI Online Judge
+ * @Categories: PARADIGMS
+ * @Problem: 1034 - Ice Statues Festival
+ * @Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1034
+ * @Timelimit: 3 sec
+ * @Status: 
+ * @Submission:
+ * @Runtime:
+ * @Solution:
+ * @Note:
+ */
+
+import java.io.*;
+
+public class P1034_Ice_Statues_Festival {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String[] st = br.readLine().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+
+        }
+        bw.flush();
+    }
+
+}
