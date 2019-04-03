90c56f2442a993986ed238e938d234cb273a6682
Teerapat <boombarm@gmail.com>
2019-04-03 22:48:06 +0700

    2019-04-03

--- src/CODEFORCES/Trying/Main.java
+++
@@ -1,7 +0,0 @@
-package CODEFORCES.Trying;
-
-/**
- * Created by Teerapat-BSD on 10/1/2018.
- */
-public class Main {
-}
--- src/HACKERRANK/Trying/Main.java
+++
@@ -1,7 +0,0 @@
-package HACKERRANK.Trying;
-
-/**
- * Created by Teerapat-BSD on 9/21/2018.
- */
-public class Main {
-}
